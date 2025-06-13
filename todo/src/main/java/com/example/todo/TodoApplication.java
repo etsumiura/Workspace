126530a0f7c1569a68e659adf63040a4b85ea51f
etsu.miura <etsu.miura@avantcorp.com>
2025-06-13 17:45:13 +0900

    kko

--- todo/src/main/java/com/example/todo/TodoApplication.java
+++ todo/src/main/java/com/example/todo/TodoApplication.java
@@ -1,3 +1,8 @@
+package com.example.todo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
 @SpringBootApplication
 public class TodoApplication {
     public static void main(String[] args) {
